5de54b339a2864a172c75a4332472dee7c7883d8
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2018-05-08 21:13:03 -0300

    Mudanças no cutoff

--- cutoff.py
+++ cutoff.py
@@ -7,7 +7,8 @@ func = 0
 def cutoff_quant():
     print("Quantidade de vezes que a função foi chamada =", func)
     print("Quantidade de cutoffs =", quant)
-    print("Porcentagem =", quant/func)
+    if func > 0:
+        print("Porcentagem =", quant/func)
 
 
 def cutoff_quant_zera():
@@ -31,10 +32,14 @@ def cutoff(ia, contador, eh_min):
     for i in range(len(jogo)):
         for j in range(0, len(jogo[i]) - 4):
             n = min(jogo[i][j:j+4]) + max(jogo[i][j:j+4])
-            if n < 0:
-                possibilidades_ia += 1
-            elif n > 0:
-                possibilidades_jogador += 1
+            if n < 0 and jogo[i][j:j+4].count(0) == 1:
+                possibilidades_ia += 7
+            elif n > 0 and jogo[i][j:j+4].count(0) == 1:
+                possibilidades_jogador += 7
+            elif n < 0 and jogo[i][j:j + 4].count(0) == 2:
+                possibilidades_ia += 3
+            elif n > 0 and jogo[i][j:j + 4].count(0) == 2:
+                possibilidades_jogador += 3
 
     # Colunas
     for j in range(len(jogo[0])):
@@ -45,50 +50,49 @@ def cutoff(ia, contador, eh_min):
                 temp.append(jogo[k][j])
 
             n = min(temp) + max(temp)
-            if n < 0:
-                possibilidades_ia += 1
-            elif n > 0:
-                possibilidades_jogador += 1
+            if n < 0 and temp.count(0) == 1:
+                possibilidades_ia += 7
+            elif n > 0 and temp.count(0) == 1:
+                possibilidades_jogador += 7
+            elif n < 0 and temp.count(0) == 2:
+                possibilidades_ia += 3
+            elif n > 0 and temp.count(0) == 2:
+                possibilidades_jogador += 3
 
     # Diagonais cima pra baixo
     for i in range(len(jogo) - 3):
         for j in range(len(jogo[i]) - 3):
             temp = diagonal_cima_baixo(jogo, i, j)
             n = min(temp) + max(temp)
-            if n < 0:
-                possibilidades_ia += 1
-            elif n > 0:
-                possibilidades_jogador += 1
+            if n < 0 and temp.count(0) == 1:
+                possibilidades_ia += 7
+            elif n > 0 and temp.count(0) == 1:
+                possibilidades_jogador += 7
+            elif n < 0 and temp.count(0) == 2:
+                possibilidades_ia += 3
+            elif n > 0 and temp.count(0) == 2:
+                possibilidades_jogador += 3
 
     #Diagonal baixo pra cima
     for i in range(3, len(jogo)):
         for j in range(len(jogo[i]) - 3):
             temp = diagonal_baixo_cima(jogo, i, j)
             n = min(temp) + max(temp)
-            if n < 0:
-                possibilidades_ia += 1
-            elif n > 0:
-                possibilidades_jogador += 1
+            if n < 0 and temp.count(0) == 1:
+                possibilidades_ia += 7
+            elif n > 0 and temp.count(0) == 1:
+                possibilidades_jogador += 7
+            elif n < 0 and temp.count(0) == 2:
+                possibilidades_ia += 3
+            elif n > 0 and temp.count(0) == 2:
+                possibilidades_jogador += 3
 
-    if possibilidades_jogador > possibilidades_ia:
+    if possibilidades_jogador > 2.0*possibilidades_ia:
         global quant
         quant += 1
         return True
     else:
         return False
-
-    if eh_min:  # Se estiver calculando o mínimo, ou seja, simulando a jogada do jogador
-        # Se a IA possir mais jogadas que o jogador, ativa o cutoff
-        if possibilidades_jogador < possibilidades_ia:
-            return True
-        else:
-            return False
-    else:  # Se estiver calculando o max, ou seja, simulando jogada da IA
-        # Se o jogador tiver mais possibilidades que a IA, ativa o cutoff
-        if possibilidades_jogador > possibilidades_ia:
-            return True
-        else:
-            return False
 
 # Retorna uma lista com os valores da diagonal de cima pra baixo (\)
 def diagonal_cima_baixo(jogo, i, j):
