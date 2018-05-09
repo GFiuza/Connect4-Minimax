fad70d20128b1fb959c577d90a4b25a65ed09759
ddszb <38837749+ddszb@users.noreply.github.com>
2018-05-08 22:31:59 -0300

    Add files via upload
    Adicionado comentários ao código

--- Connect4_cutoff.py
+++ Connect4_cutoff.py
@@ -3,49 +3,49 @@ import sys
 from Jogo import Jogo
 from Jogo import bcolors
 from minimax_cutoff import Int_Art
-# from minimax import Int_Art
-# from minimax_poda import IntArt
-
 clear = lambda: os.system('cls')
+                                                                                                # Esse comando foi inserida para evitar
+#sys.setrecursionlimit(1500)                                                                    # que a pilha estourasse antes
+                                                                                                # da implementacao das buscas.
+                                                                                                # Após as estratégicas de poda e
+                                                                                                # heurística, a parada da recursão
+                                                                                                # mais rápida parece ter restringido
+                                                                                                # o estouro.
 
-sys.setrecursionlimit(1500)
+def mainLoop():                                                                                 # Cria uma instancia de Jogo
+    xogao = Jogo()                                                                              # Cria uma instancia de IntArt
+    computador = Int_Art()                                                                       # Cria uma cópia do jogo para passar
+    computador.jogo_copia = xogao                                                               # dentro do loop
+    xogao.imprime()                                                                             # printa o estado atual
 
+    while True:                                                                                 # Escopo do loop principal
+        xogao.imprime_vez()                                                                     #
+                                                                                                #
+        entrada = int(input(bcolors.BOLD + bcolors.VERDE + "Sua vez: "))                        # Lê a entrada do jogador humano.
+                                                                                                #
+        while entrada > 6 or entrada < 0 or xogao.ondeLivre[entrada] < 0:                       # Limita a entrada de inteiros para valores
+            entrada = int(input(bcolors.BOLD + bcolors.VERMELHO + "Diga uma coluna válida: "))  # válidos
+            if entrada <= 6 and entrada >= 0 and xogao.ondeLivre[entrada] < 0:                  # Se uma coluna está cheia, não pode inserir
+                continue                                                                        # uma peça nela
 
-def mainLoop():
-    xogao = Jogo()
-    computador = Int_Art()
-    computador.jogo_copia = xogao
-    xogao.imprime()
+        xogao.coloca_disco(entrada)                                                             # Chama a função de Jogo para inserir uma
+        clear()                                                                                 # peça no tabuleiro
+        os.system('cls')                                                                        #
+        xogao.imprime()                                                                         # Imprime o estado atual
 
-    while True:
+        if xogao.alguemGanhou() != 0 or xogao.cheio():                                          # Verifica se há alguma condição de parada
+            xogao.imprime_vencedor()                                                            # para dar um break no loop
+            break                                                                               #
+
         xogao.imprime_vez()
 
-        entrada = int(input(bcolors.BOLD + bcolors.VERDE + "Sua vez: "))
-
-        while entrada > 6 or entrada < 0 or xogao.ondeLivre[entrada] < 0:
-            entrada = int(input(bcolors.BOLD + bcolors.VERMELHO + "Diga uma coluna válida: "))
-            if entrada <= 6 and entrada >= 0 and xogao.ondeLivre[entrada] < 0:
-                continue
-
-        xogao.coloca_disco(entrada)
-        clear()
+        xogao.coloca_disco(computador.minimax_retorno())                                         # Já com a vez invertida, chama a funcao
+        clear()                                                                                 # de colocar disco para a instancia de IA
         os.system('cls')
         xogao.imprime()
 
-        if xogao.alguemGanhou() != 0 or xogao.cheio():
-            xogao.imprime_vencedor()
-            break
+        if xogao.alguemGanhou() != 0 or xogao.cheio():                                          # Verifica novamente se houve uma
+            xogao.imprime_vencedor()                                                            # condicao de parada (empate ou vitoria)
+            break                                                                               # e faz um break se houver.
 
-        xogao.imprime_vez()
-
-        xogao.coloca_disco(computador.minimax_retorno())
-        clear()
-        os.system('cls')
-        xogao.imprime()
-
-        if xogao.alguemGanhou() != 0 or xogao.cheio():
-            xogao.imprime_vencedor()
-            break
-
-
-mainLoop()
+mainLoop()                                                                                      # chama o mainLoop()
